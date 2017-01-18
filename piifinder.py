48355ec953f4c9f61baa4a3d5c44b692c10627bf
Icefyre <Icefyre@tektome.com>
2017-01-18 09:59:33 -0500

    added some test files, working on file support

--- piifinder.py
+++ piifinder.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python
-
-
 import re
 import sys
 import zipfile
+import os
 ssnPattern = "\d{3}-\d{2}-\d{4}"
 
+'''
+myfiles = os.walk("/cygdrive/s/2016")
+
+for item in myfiles:
+    for filelist_item in item[2]:
+        if "xlsx" in filelist_item:
+            print "%s%s%s" % (item[0],os.path.sep,filelist_item)
+'''        
+
+   
 fileName = sys.argv[1]
-if sys.argv[1].split(".")[1] == "xlsx":
+if sys.argv[1].split(".")[1] == "xlsx" or sys.argv[1].split(".")[1] == "docx":
     with zipfile.ZipFile(fileName) as myfile:
 
+        ssnList = []
         files = myfile.namelist()
-
+        print files
         for zfile in files:
             data = myfile.read(zfile)
             matches = re.findall(ssnPattern,data)
             if len(matches) > 0:
-                print "%s: %s" % (fileName, matches)
-            
-#        for data in zipfile.ZipFile.read(myfile,"r"):
- #           print data
-        #for archiveFile in  myfile.namelist():
-#         matches = re.findall(ssnPattern,zipfile.ZipFile.open(archiveFile))
-         #   print matches
+                ssnList += matches
+        uniqssn = list(set(ssnList))
+        print "%s: %s" % (fileName, uniqssn)
+        print "number of matches =  %d" % len(uniqssn)
 else:
     with open(sys.argv[1]) as myfile:
         matches = re.findall(ssnPattern,myfile.read())
         print matches
+
